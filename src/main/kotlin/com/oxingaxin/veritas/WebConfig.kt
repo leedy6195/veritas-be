ece117059ae0e6e2c571c7e9f110dfee7b9c3f67
Dongyun <leedy6195@naver.com>
2024-04-28 06:13:31 +0900

    github actions test

--- src/main/kotlin/com/oxingaxin/veritas/WebConfig.kt
+++ src/main/kotlin/com/oxingaxin/veritas/WebConfig.kt
@@ -3,6 +3,7 @@ package com.oxingaxin.veritas
 import com.oxingaxin.veritas.common.argumentresolver.LoggedInArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -12,4 +13,9 @@ class WebConfig : WebMvcConfigurer {
         resolvers.add(LoggedInArgumentResolver())
     }
 
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
 }
