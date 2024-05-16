55ad42b0cd1cbf7b59151d5d61355437591a3038
Dongyun <leedy6195@naver.com>
2024-05-16 14:04:42 +0900

    feat: enrollment & scheduleAttendance

--- src/main/kotlin/com/oxingaxin/veritas/lecture/controller/EnrollmentController.kt
+++ src/main/kotlin/com/oxingaxin/veritas/lecture/controller/EnrollmentController.kt
@@ -2,7 +2,9 @@ package com.oxingaxin.veritas.lecture.controller
 
 import com.oxingaxin.veritas.common.BaseResponse
 import com.oxingaxin.veritas.lecture.domain.dto.EnrollmentRequest
+import com.oxingaxin.veritas.lecture.domain.entity.Enrollment
 import com.oxingaxin.veritas.lecture.service.EnrollmentService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +16,12 @@ import org.springframework.web.bind.annotation.RestController
 class EnrollmentController(
     private val enrollmentService: EnrollmentService
 ) {
+
+    @GetMapping
+    fun getEnrollments(): BaseResponse<List<Enrollment>> {
+        val enrollments = enrollmentService.findEnrollments()
+        return BaseResponse.ok(enrollments)
+    }
     @PostMapping
     fun enroll(
             @RequestBody enrollmentRequest: EnrollmentRequest
